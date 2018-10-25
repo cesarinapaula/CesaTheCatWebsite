48438400ae649d37d4ae3b70ecc5a1449a3ec622
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-10-25 10:52:45 -0400

    Updated Things Page

--- src/Components/Things.jsx
+++ src/Components/Things.jsx
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
 
+const Center = {
+  textAlign: "center",
+  fontSize: 26,
+}
+
+const List = {
+  margin: 0,
+  padding: 5,
+  fontSize: 20 
+}
+
 class Things extends Component {
     render(){
         return(
-            <div>
-                <h1>About</h1>
+            <div style={Center}>
+                <h1>Things Cesa Likes</h1>
+                <ul style={List}>
+                    <li>Napping</li>
+
+                </ul>
             </div>
         )
     }
